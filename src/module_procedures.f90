bbb201e7b1fe4d8c57cc9b8515a00196b34fd099
Peiyang Cheng <pcheng@pchengNirvana.local>
2020-06-20 21:37:08 -0500

    Peiyang Cheng

--- src/module_procedures.f90
+++ src/module_procedures.f90
@@ -7,17 +7,14 @@ contains
     implicit none
 
     character(len = *), intent (in) :: string
-    character(len = :), allocatable :: newstring
+    character(len = 200) :: newstring
     logical :: is_numeric
     real(4) :: x
-    integer :: iostats, stringlength
+    integer :: iostats
 
-    stringlength = len(trim(adjustl(string)))
-    allocate(character(len = stringlength) :: newstring)
     newstring = trim(adjustl(string))
-    read(newstring, *, iostat = iostats) x
+    read(trim(newstring), *, iostat = iostats) x
     is_numeric = iostats == 0
-    if (allocated(newstring)) deallocate(newstring)
   end function is_numeric
 
 end module module_procedures
